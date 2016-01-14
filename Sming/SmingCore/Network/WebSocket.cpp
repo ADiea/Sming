dff5db050a0061e3ce19438371ee69916c96b4b4
unknown <diea_andrei@yahoo.com>
2016-01-14 23:52:37 +0200

    close tcp connection properly

--- Sming/SmingCore/Network/WebSocket.cpp
+++ Sming/SmingCore/Network/WebSocket.cpp
@@ -53,7 +53,7 @@ void WebSocket::send(const char* message, int length, wsFrameType type)
 	size_t headSize = sizeof(frameHeader);
 	wsMakeFrame(nullptr, length, frameHeader, &headSize, type);
 	connection->write((char*)frameHeader, headSize, TCP_WRITE_FLAG_COPY | TCP_WRITE_FLAG_MORE);
-	connection->writeString(message, TCP_WRITE_FLAG_COPY);
+	connection->write(message, length, TCP_WRITE_FLAG_COPY);
 	connection->flush();
 }
 
