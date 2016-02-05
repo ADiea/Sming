1ba3b6f42fb913fdcb336b50e6976a6dacbc8dbe
unknown <diea_andrei@yahoo.com>
2016-02-05 08:50:59 +0200

    fix vector leak;fix tcp conn already freed

--- Sming/SmingCore/SystemClock.cpp
+++ Sming/SmingCore/SystemClock.cpp
@@ -24,9 +24,6 @@ void SystemClockClass::setTime(time_t time, TimeZone timeType /* = eTZ_Local */)
 	(timeType == eTZ_UTC) ?	RTC.setRtcSeconds((time + (timezoneDiff * SECS_PER_HOUR))) : RTC.setRtcSeconds(time);
 	debugf("time updated? %d", timeSet);
 
-	if(timeSet)
-		srandom(time);
-
 	status = eSCS_Set;
 }
 
